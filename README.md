2f88cbb699235a1dd18050f1dd1d9c5b6fb05f97
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-01-30 21:44:21 -0900

    add: login.html & package manager (npm init)
    Initializer template – https://docs.npmjs.com/cli/v8/commands/npm-init

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CS 372
 1. Login Page (Node.js + MongoDB)
     - [Iteration Schedule](https://github.com/benstream/CS372/projects/1)
+    - Run in the `src` directory with `node app.js`.
+    - Install packages with `npm install`.
--- src/app.js
+++ src/app.js
@@ -5,15 +5,20 @@
 
 const http = require('http');
 
+var url = require('url');
+var fs = require('fs');
+
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('CS 372 Login Page');
+    fs.readFile('login.html', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
 });
-
+  
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
