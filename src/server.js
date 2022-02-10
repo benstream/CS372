ae8672eefb4bbf7c7fc7093711948f565d539352
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-02-10 14:30:53 -0900

    add: MongoDB login scaffolding

--- src/server.js
+++ src/server.js
@@ -3,22 +3,29 @@
 // Solomon Himelbloom
 // 2022-01-30
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://127.0.0.1:27017/';
+
 const express = require('express');
 const parser = require('body-parser');
 const bcrypt = require('bcrypt');
 
 const app = express();
 
+// app.use('/favicon.ico', express.static('staic/favicon.ico'));
+
 app.use(parser.urlencoded({ extended: true }));
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
-// TODO: Add salting and hashing with bcrypt.
+// TODO:
+// - Add salting and hashing with bcrypt.
+// - Create HTML file and then send.
 app.post('/login', (req, res) => {
 	console.log(`\n--- CREDENTIALS ---`);
 	console.log(`Plain Text Password: ${req.body.pwd}`);
-	res.send(`Username: ${req.body.uid} | Password: ${req.body.pwd}`);
+	res.send(`Username: ${req.body.uid} & Password: ${req.body.pwd}`);
 });
 
 app.get('/', (req, res) => {
