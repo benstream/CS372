caee6d946d6d2a431b4ca64255f2b6d2cd72ffc4
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-02-11 08:40:04 -0900

    add: basic login control flow (success & failure)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 1. Login Page (Node.js + MongoDB): [Iteration Schedule](https://github.com/benstream/CS372/projects/1)
 
 # Development Instructions
-- Preview project via `node server.js` in the `src` directory.
+- Preview project via `node server.js` or `npm start run dev` in the `src` directory.
 - Install relevant packages with `npm install`.
 
 # MongoDB
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const parser = require('body-parser');
 const bcrypt = require('bcrypt');
+const { redirect } = require('express/lib/response');
 
 const app = express();
 
@@ -13,11 +14,13 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017/';
 
 // ⚠️ User-specfic database & server connections:
-const projDB = 'cs372'; 
+const projDB = 'cs372';
 const projTbl = 'user';
 
 const hostname = '127.0.0.1';
 const port = 8080;
+
+const saltRounds = 10;
 
 app.use(parser.urlencoded({ extended: true }));
 
@@ -25,11 +28,15 @@ app.post('/login', (req, res) => {
 	var username = req.body.uid;
 	var password = req.body.pwd;
 
+	// TODO: Login control flow.
+	// res.redirect('/success');
+	// res.redirect('/failure');
+
 	console.log(`\n--- CREDENTIALS ---`);
 	console.log(`Plain Text Username: ${username}`);
 	console.log(`Plain Text Password: ${password}`);
 
-	res.send(`Username: ${req.body.uid} & Password: ${req.body.pwd}`);
+	res.send(`Username: ${req.body.uid} -- Password: ${req.body.pwd}`);
 
 	MongoClient.connect(url, function (err, db) {
 		if (err) throw err;
@@ -45,17 +52,17 @@ app.post('/login', (req, res) => {
 
 app.post('/registrationreq', (req, res) => {
 	var username = req.body.uid;
-	var password = req.body.pwd; //hash the passwordd here
+	var password = req.body.pwd; // TODO: hash the password here
 	var email = req.body.email;
 
-	
 	console.log(`\n--- CREDENTIALS ---`);
 	console.log(`Plain Text Email: ${email}`);
 	console.log(`Plain Text Username: ${username}`);
 	console.log(`Plain Text Password: ${password}`);
-	
 
-	res.send(`Account Registered with Email: ${email} ,Username: ${username} & Password: ${password}`);
+	res.send(
+		`Account Registered with Email: ${email} -- Username: ${username} -- Password: ${password}`
+	);
 
 	MongoClient.connect(url, function (err, db) {
 		if (err) throw err;
@@ -67,23 +74,20 @@ app.post('/registrationreq', (req, res) => {
 		//	db.close();
 		//}
 		//else {
-			var user = { email: email, uid: username, pwd: password };
-			dbo.collection(projTbl).insertOne(user, function (err, res) {
-				if (err) throw err;
-				console.log("1 document inserted");
-				db.close();
-			});
+		var user = { email: email, uid: username, pwd: password };
+		dbo.collection(projTbl).insertOne(user, function (err, res) {
+			if (err) throw err;
+			console.log('>> 1 account inserted.');
+			db.close();
+		});
 		//}
-		
 	});
-
 });
 
 app.post('/passwordreset', (req, res) => {
 	var username = req.body.uid;
-	var newPassword = req.body.pwd; // hash the password here
-	//query username, if true, replace old hashed password with new hashed password
-
+	var newPassword = req.body.pwd; // TODO: hash the password here
+	// -> query username, if true, replace old hashed password with new hashed password
 });
 
 app.get('/', (req, res) => {
@@ -96,6 +100,14 @@ app.get('/registration', (req, res) => {
 
 app.get('/forgotpassword', (req, res) => {
 	res.sendFile(__dirname + '/static/forgotpass.html');
+});
+
+app.get('/success', (req, res) => {
+	res.sendFile(__dirname + '/static/success.html');
+});
+
+app.get('/failure', (req, res) => {
+	res.sendFile(__dirname + '/static/failure.html');
 });
 
 app.listen(port, hostname, () => {
