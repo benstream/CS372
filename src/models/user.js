4d63bffba63bd50a5a526c600c0b60c230d28e15
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-03-19 16:09:00 -0800

    add: user model for fetching videos

---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+// User + Media Content Connection -- MongoDB
+// Module for login, logout, and password reset.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+
+// export const name = 'user';
+
+console.log('Hello, user module!');
+
+let videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(videos);
