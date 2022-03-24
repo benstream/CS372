b7d13fdafb7e36914ea6b916b33b432442622314
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-03-24 15:52:48 -0800

    refactor: page URL slug, input id, & code comments

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const { redirect } = require('express/lib/response');
 
 const app = express();
 
-// ⚠️ User-specfic database & server connections:
+// ⚠️ User-specfic database, server, & page connections:
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017/';
 
@@ -32,6 +32,17 @@ const port = 8080;
 
 const saltRounds = 12;
 
+const staticPages = ['/registration', '/forgot', '/success', '/failure', '/exists'];
+
+const protectedPages = [
+	'/addition',
+	'/removal',
+	'/metadata',
+	'/review',
+	'/recommendation',
+	'/content'
+];
+
 app.use(parser.urlencoded({ extended: true }));
 
 // Session Management
@@ -40,6 +51,7 @@ const store = new MongoDBSession({
 	collection: 'mySessions'
 });
 
+// FIXME: Add additional fields to session cookie.
 app.use(
 	session({
 		secret: 'keyboard cat',
@@ -49,11 +61,8 @@ app.use(
 	})
 );
 
-// FIXME: Add additional fields to session cookie.
-// res.setHeader('Content-Type', 'text/html');
-// res.setHeader('Set-Cookie', 'foo=bar; Path=/; HttpOnly');
-// res.cookie('name', 'test', { domain: 'example.com', path: '/admin', secure: true });
-
+// Account Management (Login/Logout)
+// TODO: Remove session cookie upon logout.
 app.post('/login', (req, res) => {
 	MongoClient.connect(url, function (err, db) {
 		if (err) throw err;
@@ -79,6 +88,7 @@ app.post('/login', (req, res) => {
 	});
 });
 
+// New Account Creation
 app.post('/registrationreq', (req, res) => {
 	bcrypt.genSalt(saltRounds, function (err, salt) {
 		bcrypt.hash(req.body.pwd, salt, function (err, hash) {
@@ -114,6 +124,7 @@ app.post('/registrationreq', (req, res) => {
 	});
 });
 
+// Account Management (Password Reset)
 app.post('/passwordreset', (req, res) => {
 	bcrypt.genSalt(saltRounds, function (err, salt) {
 		bcrypt.hash(req.body.pwd, salt, function (err, newHash) {
@@ -132,6 +143,7 @@ app.post('/passwordreset', (req, res) => {
 	});
 });
 
+// Adding Movies
 app.post('/addition', (req, res) => {
 	MongoClient.connect(url, function (err, db) {
 		if (err) throw err;
@@ -167,6 +179,7 @@ app.post('/addition', (req, res) => {
 	});
 });
 
+// Removing Movies
 app.post('/removal', (req, res) => {
 	MongoClient.connect(url, function (err, db) {
 		if (err) throw err;
@@ -181,6 +194,7 @@ app.post('/removal', (req, res) => {
 	});
 });
 
+// Updating Movie Tags
 app.post('/metadata', (req, res) => {
 	MongoClient.connect(url, function (err, db) {
 		if (err) throw err;
@@ -199,6 +213,7 @@ app.post('/metadata', (req, res) => {
 	});
 });
 
+// Updating Movie Reviews
 app.post('/review', (req, res) => {
 	MongoClient.connect(url, function (err, db) {
 		if (err) throw err;
@@ -217,6 +232,7 @@ app.post('/review', (req, res) => {
 	});
 });
 
+// Query Movie Database [Title / Category]
 app.post('/search', (req, res) => {
 	var query = req.body.query;
 	console.log(query);
@@ -232,6 +248,7 @@ app.post('/search', (req, res) => {
 			})
 			.toArray((err, result) => {
 				if (err) throw err;
+				console.log('🔍 Search Query: ' + query);
 				console.log(result);
 				db.close();
 
@@ -244,59 +261,32 @@ app.post('/search', (req, res) => {
 	});
 });
 
+// FIXME: Session Management (Cookies)
 app.get('/', (req, res) => {
 	req.session.isAuth = true;
-	// console.log(req.session);
-	// console.log('🍪: ' + req.session.id);
+	console.log(req.session);
+	console.log('🍪: ' + req.session.id);
 	res.sendFile(__dirname + '/static/index.html');
 });
 
-// Static Pages
-app.get('/registration', (req, res) => {
-	res.sendFile(__dirname + '/static/registration.html');
+// Loading Static & Protected Pages
+staticPages.forEach((page) => {
+	app.get(page, (req, res) => {
+		res.sendFile(__dirname + '/static' + page + '.html');
+	});
 });
 
-app.get('/forgotpassword', (req, res) => {
-	res.sendFile(__dirname + '/static/forgotpass.html');
+protectedPages.forEach((page) => {
+	app.get(page, (req, res) => {
+		if (req.session.isAuth) {
+			res.sendFile(__dirname + '/protected' + page + '.html');
+		} else {
+			res.redirect('/');
+		}
+	});
 });
 
-app.get('/success', (req, res) => {
-	res.sendFile(__dirname + '/static/success.html');
-});
-
-app.get('/failure', (req, res) => {
-	res.sendFile(__dirname + '/static/failure.html');
-});
-
-app.get('/exists', (req, res) => {
-	res.sendFile(__dirname + '/static/exists.html');
-});
-
-// Protected Pages
-app.get('/content', (req, res) => {
-	res.sendFile(__dirname + '/protected/content.html');
-});
-
-app.get('/recommendation', (req, res) => {
-	res.sendFile(__dirname + '/protected/recommendation.html');
-});
-
-app.get('/review', (req, res) => {
-	res.sendFile(__dirname + '/protected/review.html');
-});
-
-app.get('/addition', (req, res) => {
-	res.sendFile(__dirname + '/protected/addition.html');
-});
-
-app.get('/removal', (req, res) => {
-	res.sendFile(__dirname + '/protected/removal.html');
-});
-
-app.get('/metadata', (req, res) => {
-	res.sendFile(__dirname + '/protected/metadata.html');
-});
-
+// Local Server Output
 app.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
